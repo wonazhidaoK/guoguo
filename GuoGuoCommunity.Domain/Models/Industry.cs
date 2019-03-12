a66c122a61904c857f8671bbdd446b5317571b30
高洁 <1798287688@qq.com>
2019-03-12 21:56:01 +0800

    增加业主信息，业户信息实体

---
+++ GuoGuoCommunity.Domain/Models/Industry.cs
@@ -0,0 +1,73 @@
+﻿using GuoGuoCommunity.Domain.Abstractions.Models;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GuoGuoCommunity.Domain.Models
+{
+    /// <summary>
+    /// 业户信息
+    /// </summary>
+    public class Industry : IDeleted, ILastOperation, ICreateOperation
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// 业户门号
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 层数
+        /// </summary>
+        public string NumberOfLayers { get; set; }
+
+        /// <summary>
+        /// 面积
+        /// </summary>
+        public string Acreage { get; set; }
+
+        /// <summary>
+        /// 朝向
+        /// </summary>
+        public string Oriented { get; set; }
+
+        /// <summary>
+        /// 楼宇Id
+        /// </summary>
+        [Required]
+        public string BuildingId { get; set; }
+
+        /// <summary>
+        /// 楼宇名称
+        /// </summary>
+        public string BuildingName { get; set; }
+
+        /// <summary>
+        /// 楼宇单元Id
+        /// </summary>
+        public string BuildingUnitId { get; set; }
+
+        /// <summary>
+        /// 楼宇单元名称
+        /// </summary>
+        public string BuildingUnitName { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public DateTimeOffset? DeletedTime { get; set; }
+
+        public string LastOperationUserId { get; set; }
+
+        public DateTimeOffset? LastOperationTime { get; set; }
+
+        public string CreateOperationUserId { get; set; }
+
+        public DateTimeOffset? CreateOperationTime { get; set; }
+    }
+}
---
+++ GuoGuoCommunity.Domain/Models/Owner.cs
@@ -0,0 +1,67 @@
+﻿using GuoGuoCommunity.Domain.Abstractions.Models;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GuoGuoCommunity.Domain.Models
+{
+    /// <summary>
+    /// 业主信息
+    /// </summary>
+    public class Owner : IDeleted, ILastOperation, ICreateOperation
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// 姓名
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 生日
+        /// </summary>
+        public string Birthday { get; set; }
+
+        /// <summary>
+        /// 性别
+        /// </summary>
+        public string Gender { get; set; }
+
+        /// <summary>
+        /// 手机号
+        /// </summary>
+        public string PhoneNumber { get; set; }
+
+        /// <summary>
+        /// 身份证
+        /// </summary>
+        public string IDCard { get; set; }
+
+        /// <summary>
+        /// 业户Id
+        /// </summary>
+        public string IndustryId { get; set; }
+
+        /// <summary>
+        /// 业户名称
+        /// </summary>
+        public string IndustryName { get; set; }
+
+        public string LastOperationUserId { get; set; }
+
+        public DateTimeOffset? LastOperationTime { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public DateTimeOffset? DeletedTime { get; set; }
+
+        public string CreateOperationUserId { get; set; }
+
+        public DateTimeOffset? CreateOperationTime { get; set; }
+    }
+}
