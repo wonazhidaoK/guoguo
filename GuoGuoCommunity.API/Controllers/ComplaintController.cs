2d92ece7c05d3bf26280e9c27ce7968a457df2b5
1798287688@qq.com <1798287688@qq.com>
2019-03-31 17:05:15 +0800

    投诉基本设计

---
+++ GuoGuoCommunity.API/Controllers/ComplaintController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace GuoGuoCommunity.API.Controllers
+{
+    public class ComplaintController : ApiController
+    {
+        /* 新增投诉需要提供接口
+         * 1.提供用户所有小区集合
+         * 2.提供用户小区下业户集合
+         * 3.提供业主用上级投诉部门集合
+         * 4.提供业主委员会用上级投诉部门集合
+         */
+         /* 1.业主新增投诉
+          * 2.业主跟进投诉接口
+          * 3.业主向上级街道办申诉接口
+          * 4.业主关闭投诉接口
+          * 
+          */
+    }
+}
